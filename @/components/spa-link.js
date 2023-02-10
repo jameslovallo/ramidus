bb9e05d7c9e037a55d92ea3bddb04515c24774c9
James Lovallo <patrickonparker@gmail.com>
2023-02-10 02:47:35 -0500

    improve router efficiency

--- @/components/spa-link.js
+++ @/components/spa-link.js
@@ -3,13 +3,13 @@ import ardi, { html } from 'https://unpkg.com/ardi'
 ardi({
   component: 'spa-link',
   props: { href: [String, '/home.html'] },
+  state: () => ({ pageData: '' }),
   template() {
     return html`
       <a
         part="link"
         href=${this.href}
         @mouseover=${(e) => this.hover(e)}
-        @mouseleave=${() => (router.pageData = '')}
         @click=${(e) => this.click(e)}
       >
         <slot></slot>
@@ -22,13 +22,30 @@ ardi({
   pagePath() {
     return (this.href !== '/' ? this.href : '') + '/index.html'
   },
+  getPage(setPage) {
+    if (!this.pageData) {
+      fetch(this.pagePath())
+        .then((res) => res.text())
+        .then((html) => {
+          this.pageData = html
+            .split('<app-layout>')[1]
+            .split('</app-layout>')[0]
+          setPage && this.setPage()
+        })
+    }
+  },
+  setPage() {
+    if (this.pageData) {
+      router.setPage(this.pageData)
+      this.pushHistory(this.pagePath())
+    } else this.getPage(true)
+  },
   hover(e) {
     e.preventDefault()
-    router.getPage(this.pagePath())
+    this.getPage()
   },
   click(e) {
     e.preventDefault()
-    router.setPage(this.pagePath())
-    this.pushHistory(this.pagePath())
+    this.setPage()
   },
 })
--- @/components/spa-router.js
+++ @/components/spa-router.js
@@ -2,28 +2,12 @@ import ardi, { html } from 'https://unpkg.com/ardi'
 
 ardi({
   component: 'spa-router',
-  state: () => ({ pageData: '' }),
   template() {
     return html`<slot></slot>`
   },
-  getPage(href, setPage) {
-    fetch(href)
-      .then((res) => res.text())
-      .then((html) => {
-        this.pageData = html.split('<app-layout>')[1].split('</app-layout>')[0]
-        if (setPage) this.setPage(href)
-      })
-  },
-  setPage(href) {
-    if (this.pageData) {
-      this.parse(this.pageData)
-      this.pageData = ''
-    } else this.getPage(href, true)
-    this.setTitle()
-  },
-  parse(text) {
+  parse(text, firstLoad) {
     if (text.trim().startsWith('#')) {
-      import('https://unpkg.com/marked@4.2.12/lib/marked.esm.js').then((m) => {
+      import('/@/lib/marked@4.2.12.esm.js').then((m) => {
         text = text
           .split('\n')
           .map((line) => line.trim())
@@ -31,7 +15,13 @@ ardi({
         text = m.parse(text)
         this.innerHTML = text
       })
-    } else this.innerHTML = text
+    } else if (!firstLoad) {
+      this.innerHTML = text
+    }
+  },
+  setPage(pageData) {
+    if (pageData) this.parse(pageData)
+    this.setTitle()
   },
   setTitle() {
     let children = this.children
@@ -46,7 +36,7 @@ ardi({
   ready() {
     window.router = this
     this.setTitle()
-    this.parse(document.querySelector('app-layout').innerHTML)
+    this.parse(document.querySelector('app-layout').innerHTML, true)
     // history stuff
     const href = location.pathname
     history.pushState({ page: href }, '', href.replace('index.html', ''))
