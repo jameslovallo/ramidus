3b00b53e25802a0dc6e25bae22aec36ae6bb26fa
James Lovallo <patrickonparker@gmail.com>
2023-02-10 23:02:44 -0500

    fix import

--- @/main.js
+++ @/main.js
@@ -1,5 +1,6 @@
 import '/@/components/app-footer.js'
 import '/@/components/app-layout.js'
 import '/@/components/app-nav.js'
+import '/@/components/code-demo.js'
 import '/@/components/spa-link.js'
 import '/@/components/spa-router.js'
