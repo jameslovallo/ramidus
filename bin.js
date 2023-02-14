3fadd1f91eab08a311752e6e3edc31fa84cd7f4c
James Lovallo <patrickonparker@gmail.com>
2023-02-14 14:36:25 -0500

    fix to target correct folder

--- bin.js
+++ bin.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 import extract from 'extract-zip'
-import fs, { readdir } from 'fs'
+import fs from 'fs'
 import https from 'https'
 import path from 'path'
 
@@ -30,25 +30,18 @@ https
           try {
             await extract(zip, { dir: path.resolve('./') })
             rm(zip)
-            readdir('./', { withFileTypes: true }, (err, items) => {
-              if (err) {
-                console.log(err)
-              } else if (items) {
-                const dir = items.filter((item) => item.isDirectory())[0].name
-                fs.cp(`./${dir}`, './', { recursive: true }, function (err) {
-                  err && console.log(err)
-                  fs.rm(dir, { recursive: true })
-                  fs.rm('./node_modules', { recursive: true })
-                  rm('.gitignore')
-                  rm('./bin.js')
-                  rm('./package.json')
-                  fs.writeFile(
-                    './package.json',
-                    pkgJSON,
-                    (err) => err && console.error(err)
-                  )
-                })
-              }
+            fs.cp('./ramidus-main', './', { recursive: true }, function (err) {
+              err && console.log(err)
+              fs.rm('./ramidus-main', { recursive: true })
+              fs.rm('./node_modules', { recursive: true })
+              rm('.gitignore')
+              rm('./bin.js')
+              rm('./package.json')
+              fs.writeFile(
+                './package.json',
+                pkgJSON,
+                (err) => err && console.error(err)
+              )
             })
           } catch (err) {
             err && console.log(err)
