75d7e94aa5cf011049ab953d5269e62573fac7d9
James Lovallo <patrickonparker@gmail.com>
2023-02-21 15:32:31 -0500

    fix title matching for pre-built html headings with IDs

--- @/components/spa-root.js
+++ @/components/spa-root.js
@@ -49,11 +49,10 @@ ardi({
     let mdH1 = doc.match(/# .+/)
     if (mdH1) mdH1 = mdH1[0].replace('# ', '')
     let htmlH1 = doc.match(/<h1.+<\/h1>/)
-    if (htmlH1) htmlH1 = htmlH1[0].replace(/<h1.+>/, '').replace('</h1>', '')
+    if (htmlH1) htmlH1 = htmlH1[0].replace(/<h1.*?>/, '').replace('</h1>', '')
     let htmlTitle = doc.match(/<title>.+<\/title>/)
     if (htmlTitle)
       htmlTitle = [0].replace('<title>', '').replace('</title>', '')
-    console.log(doc, htmlTitle, htmlH1, mdH1)
     document.title = htmlTitle || htmlH1 || mdH1
   },
   handleMD(doc) {
